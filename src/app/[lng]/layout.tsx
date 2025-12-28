d1c3debcf9d0f5565973cf82e8b00bd0d039e39d
cakasuma <amammustofa@gmail.com>
2025-12-28 22:58:54 +0800

    chore: update favicons and change middleware to proxy

--- src/app/[lng]/layout.tsx
+++ src/app/[lng]/layout.tsx
@@ -135,9 +135,9 @@ export default function RootLayout({
         />
 
         {/* Favicon and app icons */}
-        <link rel="icon" href="/favicon.ico" sizes="32x32" />
-        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="icon" href="/favicons/favicon.ico" sizes="32x32" />
+        <link rel="icon" href="/favicons/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/favicons/apple-touch-icon.png" />
         <link rel="manifest" href="/manifest.json" />
 
         {/* Theme color for mobile browsers */}
